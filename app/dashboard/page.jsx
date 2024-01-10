c6b1b4adfdb8a6aa3ff24f47a48977544c4106f2
Kurtis Garcia <kurtisgarcia14@gmail.com>
2024-01-10 02:11:39 -0500

    added some styling to the dashboard page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,6 +4,8 @@ import styles from '../../styles/dashboard.module.css'
 import Cookies from 'js-cookie';
 import { useState, useEffect } from "react";
 import Nav from '../components/Nav';
+import Image from 'next/image';
+
 
 const Dashboard = () => {
     const [userId, setUserId] = useState('');
@@ -79,22 +81,26 @@ const Dashboard = () => {
     }
 
     return ( 
-        <main className={styles.dashboardContainer}>
-            <div className={styles.dashboardWrapper}>
+        <main className={styles.container}>
+            <div className={styles.wrapper}>
                 <Nav />
-                <div>
+                <div className={styles.contentContainer}>
                     <div className={styles.content}>
-                        <p className={styles.p}>Dashboard</p>
-                        <p>email: {email}</p>
-                        <p>user id: {userId}</p>
-                        <ul>
+                        <h2 className={styles.header}>Overview</h2>
+                        <ul className={styles.ul}>
                             {properties.map(property => (
-                                <li key={property.id}>
-                                    <p>{property.street}</p>
-                                    <p>{property.city}</p>
-                                    <p>{property.state}</p>
-                                    <p>{property.zip}</p>
-                                    <img src={property.property_image} alt='' width='150px' />
+                                <li className={styles.li} key={property.id}>
+                                    <div className={styles.propertyContainer}>
+                                        <div className={styles.imageContainer}>
+                                            <img className={styles.image} src={property.property_image} alt='' width='100%' />
+                                        </div>
+                                        <div className={styles.propertyContent}>
+                                            <p className={styles.address}>{property.street}</p>
+                                            <p>{property.city}</p>
+                                            <p>{property.state}</p>
+                                            <p>{property.zip}</p>
+                                        </div>
+                                    </div>
                                 </li>
                             ))}
                         </ul>
