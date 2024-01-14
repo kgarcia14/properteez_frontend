21ca9c01cb4b199d2bfbaa71a6fc6263de18a32e
Kurtis Garcia <kurtisgarcia14@gmail.com>
2024-01-13 19:01:53 -0500

    added an array of abbreviated states to map through inside of a select input

--- app/addProperty/page.jsx
+++ app/addProperty/page.jsx
@@ -24,6 +24,8 @@ const AddProperty = () => {
     const [rentStatus, setRentStatus] = useState('');
     const [propertyImage, setPropertyImage] = useState('');
 
+    const states = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'];
+
     useEffect(() => {
         const checkForCookies = () => {
             if (!Cookies.get('id')) {
@@ -112,12 +114,18 @@ const AddProperty = () => {
                 </label>
                 <label className={styles.label}>
                     State
-                    <input className={styles.input}
+                    <select
+                    className={styles.select} 
                     type="text"
                     name="state"
                     value= {state}
                     onChange= {(e) => setState(e.target.value)}
-                    required />
+                    required>
+                        <option value="">Select State</option>
+                        {states.map(state => (
+                            <option value={state}>{state}</option>
+                        ))}
+                    </select>
                 </label>
                 <label className={styles.label}>
                     Zip
@@ -146,7 +154,7 @@ const AddProperty = () => {
                 <label className={styles.label}>
                     Mortage Amount
                     <input className={styles.input} 
-                    type="text"
+                    type="number"
                     name="mortgageAmount"
                     value= {mortgageAmount}
                     onChange= {(e) => setMortgageAmount(e.target.value)} />
@@ -201,13 +209,21 @@ const AddProperty = () => {
                     onChange= {(e) => setRenterEmail(e.target.value)} />
                 </label>
                 <label className={styles.label}>
-                    Lease Term
+                    Lease Start Date
                     <input className={styles.input} 
-                    type="text"
+                    type="date"
                     name="leaseTerm"
                     value= {leaseTerm}
                     onChange= {(e) => setLeaseTerm(e.target.value)} />
                 </label>
+                {/* <label className={styles.label}>
+                    Lease End Date
+                    <input className={styles.input} 
+                    type="date"
+                    name="leaseTerm"
+                    value= {leaseTerm}
+                    onChange= {(e) => setLeaseTerm(e.target.value)} />
+                </label> */}
                 <label className={styles.label}>
                     Rent Amount
                     <input className={styles.input} 
